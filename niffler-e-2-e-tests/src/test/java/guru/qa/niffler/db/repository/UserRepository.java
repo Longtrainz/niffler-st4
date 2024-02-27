5c9159d9412251e61027678741895de89326eab5
vladbukach <vladyslav.bukach@airslate.com>
2024-02-27 16:29:26 +0200

    Hw 3

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/db/repository/UserRepository.java
@@ -0,0 +1,25 @@
+package guru.qa.niffler.db.repository;
+
+import guru.qa.niffler.db.model.UserAuthEntity;
+import guru.qa.niffler.db.model.UserEntity;
+
+import java.util.UUID;
+
+public interface UserRepository {
+
+    UserAuthEntity createInAuth(UserAuthEntity user);
+
+    UserEntity createInUserdata(UserEntity user);
+
+    void deleteInAuthById(UUID id);
+
+    void deleteInUserdataById(UUID id);
+
+    UserAuthEntity readInAuth(String username);
+
+    UserEntity readInUserdata(String username);
+
+    UserAuthEntity updateInAuth(UserAuthEntity user);
+
+    UserEntity updateInUserdata(UserEntity user);
+}
